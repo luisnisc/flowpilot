2ddbd96f8befd4f4519eb593182558b0a6c1fa02
luisnisc <luisnisc30mayo2005@gmail.com>
2025-04-30 13:08:45 +0200

    feat: enhance DashBoard layout and add task/project sections; update Home page with login button

--- src/app/components/DashBoard.tsx
+++ src/app/components/DashBoard.tsx
@@ -1,21 +1,21 @@
-"use client"
-import { useSession, signOut } from "next-auth/react"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
-import SideBar from "./SideBar"
+"use client";
+import { useSession, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import SideBar from "./SideBar";
 
 export default function DashBoard() {
-  const { data: session, status } = useSession()
-  const router = useRouter()
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
   useEffect(() => {
     if (status === "unauthenticated") {
-      router.push("/login")
+      router.push("/login");
     }
-  }, [status, router])
+  }, [status, router]);
 
   if (status === "loading") {
-    return <div>Cargando...</div>
+    return <div>Cargando...</div>;
   }
 
   return (
@@ -25,18 +25,35 @@ export default function DashBoard() {
           onClick={() =>
             signOut({ callbackUrl: "http://localhost:3000/login" })
           }
-
           className="btn btn-primary"
         >
           Logout
         </button>
       </div>
-      <SideBar/>
-      <div className="flex flex-col h-screen bg-gray-200 text-black *:w-5/6 ml-[19.66667%]">
-        <h1 className="text-3xl font-bold mb-4">Bienvenido
-            
-        </h1>
+      <SideBar />
+      <main className="flex flex-col h-screen bg-gray-200 text-black ml-[19.66667%]">
+      <div>
+        <h1 className="text-black text-5xl text-center mt-10">Bienvenido a FlowPilot</h1>
       </div>
+      <div className="h-screen bg-gray-200 text-black grid grid-cols-2 gap-4 p-10">
+        <div id="tareasAsignadas" className="flex flex-col items-center">
+          <h3 className="text-xl font-bold text-center mt-10">
+            Tareas Asignadas
+          </h3>
+          <div className="flex flex-col items-center justify-center mt-10 border h-96 w-full bg-white rounded-lg shadow-lg">
+            <p className="text-lg"></p>
+          </div>
+        </div>
+        <div id="proyectosActuales" className="flex flex-col items-center">
+          <h3 className="text-xl font-bold mt-10">
+            Proyectos Actuales
+          </h3>
+          <div className="flex flex-col items-center justify-center mt-10 border h-96 w-full bg-white rounded-lg shadow-lg">
+            <p className="text-lg"></p>
+          </div>
+        </div>
+      </div>
+      </main>
     </div>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="text-black flex min-h-screen flex-col items-center justify-between p-96">
       <h1 className="text-4xl font-bold">Bienvenido a FlowPilot</h1>
       <p className="mt-4 text-lg">La nueva solución para la gestión de proyectos en tiempo real</p>
+      <a href="/login"><button className='btn btn-rpimary'>Empezar</button></a>
     </main>
   )
 }
