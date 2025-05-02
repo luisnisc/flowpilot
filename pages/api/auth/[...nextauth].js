07bd779c8a3d1fb8df5e32990d3a9ee9ed60f76b
luisnisc <luisnisc30mayo2005@gmail.com>
2025-05-02 11:12:39 +0200

    fix: update MongoDB adapter import path to correct package

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
 import bcrypt from "bcryptjs";
 import GoogleProvider from "next-auth/providers/google";
